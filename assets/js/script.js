6d4236623c875b377fae031ff5c8fb715d46284c
Pawel Kozicki <pe.kozicki@gmail.com>
2017-10-11 20:20:45 +0200

    Add basic functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log("Working");
+$(() => {
+    // Strike through particular todos on click
+    $("ul").on("click", ".todo", function() {        
+        $(this).toggleClass("completed");
+    });
+
+    // Click on X to delete todo
+    $("ul").on("click", ".delete", function(e) {
+        $(this).parent().fadeOut(500, function() {
+            $(this).remove();
+        });
+        e.stopPropagation();
+    });
+
+    $("#newItem").keypress(function(e) {
+        if(e.which === 13) {
+            const todoText = $(this).val();
+            $("ul").append('<li class="todo"><span class="delete">X</span> ' + todoText + '</li>');
+        }
+    })
+    
+});
