a58a69cef59c658c99c33254c59fba6ad0a98511
Pawel Kozicki <pe.kozicki@gmail.com>
2017-10-12 18:51:28 +0200

    Add functionalities to newly added buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 $(() => {
-    // Strike through particular todos on click
-    $("ul").on("click", ".todo", function() {        
-        $(this).toggleClass("completed");
+    // Strike through particular todos on check
+    $("ul").on("click", ".checkbox", function(e) {        
+        let $this = $(this);
+        $this.parent().toggleClass("completed");
+        $this.toggleClass("fa-square-o fa-check-square-o");
+        e.stopPropagation();
     });
 
-    // Click on X to delete todo
+    // Click on trashcan to delete an item
     $("ul").on("click", ".delete", function(e) {
         $(this).parent().fadeOut(500, function() {
             $(this).remove();
@@ -12,16 +15,29 @@ $(() => {
         e.stopPropagation();
     });
 
+    // Click on eraser to delete all completed items
+    $("i.fa-eraser").click(function() {
+        $(".completed").fadeOut(500, function() {
+            $(this).remove();
+        });
+    });
+
+    // Add new items
     $("#newItem").keypress(function(e) {
         if(e.which === 13) {
             const todoText = $(this).val();
             $(this).val("");
-            $("ul").append('<li class="todo"><span class="delete"><i class="fa fa-trash-o" aria-hidden="true"></i></span> ' + todoText + '</li>');
+            $("ul").append(
+                '<li class="todo"><span class="delete"><i class="fa fa-trash-o" aria-hidden="true"></i></span> ' + todoText + '<i class="checkbox fa fa-square-o" aria-hidden="true"></li>');
         }
     });
 
-    $(".fa-plus-square-o").click(function() {
-        $("#newItem").fadeToggle();
+    // Show/hide adding new item field
+    $(".show-field").click(function() {
+        let $this = $(this);
+        $("#newItem").fadeToggle(500, function() {
+            $this.toggleClass("fa-minus-square-o fa-plus-square-o");
+        });        
     });
     
 });
